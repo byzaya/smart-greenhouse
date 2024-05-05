9ec1f67c51518511ebff5c718783b3e796c9240e
byzaya <byzaya777@gmail.com>
2024-05-06 00:53:18 +0300

    fix @RestController

--- src/main/java/org/greenhouse/controller/greenhouse/ControlController.java
+++ src/main/java/org/greenhouse/controller/greenhouse/ControlController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+@RestController
 @RequestMapping("/api/v1/control")
 @RequiredArgsConstructor
 @Tag(name = "Информация о приборах")
