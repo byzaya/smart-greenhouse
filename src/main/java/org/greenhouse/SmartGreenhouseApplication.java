378649b17c49e138da394b3690111e5a7a7d2e26
byzaya <byzaya777@gmail.com>
2024-05-09 18:52:09 +0300

    add config and delete some strings

--- src/main/java/org/greenhouse/SmartGreenhouseApplication.java
+++ src/main/java/org/greenhouse/SmartGreenhouseApplication.java
@@ -2,12 +2,25 @@ package org.greenhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
 public class SmartGreenhouseApplication {
   public static void main(String[] args) {
     SpringApplication.run(SmartGreenhouseApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+      }
+    };
   }
 }
