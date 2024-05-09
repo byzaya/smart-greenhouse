42cced5c2c4a1c89de7a9d54a1851ad238bd8ac0
byzaya <byzaya777@gmail.com>
2024-05-09 19:18:04 +0300

    8080

--- src/main/java/org/greenhouse/config/OpenApiConfig.java
+++ src/main/java/org/greenhouse/config/OpenApiConfig.java
@@ -17,7 +17,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
             description = "OpenApi documentation for Smart Greenhouse",
             title = "OpenApi specification - byzaya",
             version = "1.0"),
-    servers = {@Server(description = "Local ENV", url = "http://localhost:8081")},
+    servers = {@Server(description = "Local ENV", url = "http://localhost:8080")},
     security = {@SecurityRequirement(name = "bearerAuth")})
 @SecurityScheme(
     name = "bearerAuth",
