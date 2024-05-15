65f2cf70b4a9fe772ff3e492f1f21f2ea0d8aad5
byzaya <byzaya777@gmail.com>
2024-05-15 16:00:19 +0300

    add send logs manual control

--- src/main/java/org/greenhouse/service/control/manual_control/ManualControl.java
+++ src/main/java/org/greenhouse/service/control/manual_control/ManualControl.java
@@ -1,6 +1,8 @@
 package org.greenhouse.service.control.manual_control;
 
+import java.sql.Timestamp;
 import lombok.RequiredArgsConstructor;
+import org.greenhouse.dto.input.log.SendLogInputDto;
 import org.greenhouse.entity.greenhouse.Configurations;
 import org.greenhouse.entity.greenhouse.Control;
 import org.greenhouse.entity.greenhouse.Greenhouses;
@@ -13,6 +15,7 @@ import org.greenhouse.exception.message.control_message.HeaterAlreadyOnException
 import org.greenhouse.exception.message.control_message.WateringAlreadyOnException;
 import org.greenhouse.exception.message.control_message.WindowAlreadyHalfOpenException;
 import org.greenhouse.service.ValidationService;
+import org.greenhouse.service.control.LogService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,10 +24,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class ManualControl {
 
   private final ValidationService validationService;
+  private final LogService logService;
+
+  // TODO как выяснить id топика
+  private void makeSendLog(
+      Boolean reply, Integer command, Timestamp sendTime, Long topicId) {
+    SendLogInputDto sendLogInputDto = new SendLogInputDto(reply, command, sendTime, topicId);
+    logService.sendLog(sendLogInputDto);
+  }
 
   // тут нужно записать в mqtt команду, записать в логах бд и записать в control бд
 
   // включить вентилятор в теплице
+  // command 0
   @Transactional
   public void fanOn(Long greenhouseId) {
     Greenhouses greenhouses = validationService.getGreenhouseOrThrow(greenhouseId);
@@ -35,11 +47,12 @@ public class ManualControl {
       throw new FanAlreadyOnException("Fan is already on, maybe you need to off?");
     }
     // TODO записать в MQTT
-    // TODO сделать запись о вкл вентилятора в бд
+    makeSendLog(true, 0, new Timestamp(System.currentTimeMillis()), 1L);
     greenhouses.getControl().setFanEnabled(true);
   }
 
   // выключить вентилятор в теплице
+  // command 1
   @Transactional
   public void fanOff(Long greenhouseId) {
     Greenhouses greenhouses = validationService.getGreenhouseOrThrow(greenhouseId);
@@ -50,11 +63,12 @@ public class ManualControl {
       throw new FanAlreadyOffException("Fan is already off, maybe you need to on?");
     }
     // TODO записать в MQTT
-    // TODO сделать запись о выкл вентилятора в бд
+    makeSendLog(true, 1, new Timestamp(System.currentTimeMillis()), 1L);
     greenhouses.getControl().setFanEnabled(false);
   }
 
   // включить тепловентилятор в теплице
+  // command 2
   @Transactional
   public void heaterOn(Long greenhouseId) {
     Greenhouses greenhouses = validationService.getGreenhouseOrThrow(greenhouseId);
@@ -65,11 +79,12 @@ public class ManualControl {
       throw new HeaterAlreadyOnException("Heater is already on, maybe you need to off?");
     }
     // TODO записать в MQTT
-    // TODO сделать запись о вкл тепловентилятора в бд
+    makeSendLog(true, 2, new Timestamp(System.currentTimeMillis()), 1L);
     greenhouses.getControl().setHeaterEnabled(true);
   }
 
   // выключить тепловентилятор в теплице
+  // command 3
   @Transactional
   public void heaterOff(Long greenhouseId) {
     Greenhouses greenhouses = validationService.getGreenhouseOrThrow(greenhouseId);
@@ -80,11 +95,12 @@ public class ManualControl {
       throw new HeaterAlreadyOffException("Heater is already off, maybe you need to on?");
     }
     // TODO записать в MQTT
-    // TODO сделать запись о выкл тепловентилятора в бд
+    makeSendLog(true, 3, new Timestamp(System.currentTimeMillis()), 1L);
     greenhouses.getControl().setHeaterEnabled(false);
   }
 
   // открыть окно на половину в теплице
+  // command 4
   @Transactional
   public void windowHalfOpen(Long greenhouseId) {
     Greenhouses greenhouses = validationService.getGreenhouseOrThrow(greenhouseId);
@@ -96,11 +112,12 @@ public class ManualControl {
           "Window is already half open, maybe you need to full open or close it?");
     }
     // TODO записать в MQTT
-    // TODO сделать запись об открытии окна наполовину в бд
+    makeSendLog(true, 4, new Timestamp(System.currentTimeMillis()), 1L);
     greenhouses.getControl().setWindowStatus(1);
   }
 
   // открыть окно полностью в теплице
+  // command 5
   @Transactional
   public void windowFullOpen(Long greenhouseId) {
     Greenhouses greenhouses = validationService.getGreenhouseOrThrow(greenhouseId);
@@ -112,11 +129,12 @@ public class ManualControl {
           "Window is already full open, maybe you need to half open or close it?");
     }
     // TODO записать в MQTT
-    // TODO сделать запись об открытии окна полностью в бд
+    makeSendLog(true, 5, new Timestamp(System.currentTimeMillis()), 1L);
     greenhouses.getControl().setWindowStatus(2);
   }
 
   // закрыть окно в теплице
+  // command 6
   @Transactional
   public void windowClose(Long greenhouseId) {
     Greenhouses greenhouses = validationService.getGreenhouseOrThrow(greenhouseId);
@@ -128,11 +146,12 @@ public class ManualControl {
           "Window is already close, maybe you need to half open or full open it?");
     }
     // TODO записать в MQTT
-    // TODO сделать запись об закрытии окна полностью в бд
+    makeSendLog(true, 6, new Timestamp(System.currentTimeMillis()), 1L);
     greenhouses.getControl().setWindowStatus(0);
   }
 
   // включить свет
+  // command 7
   @Transactional
   public void lightOn(Long greenhouseId) {
     Greenhouses greenhouses = validationService.getGreenhouseOrThrow(greenhouseId);
@@ -143,11 +162,12 @@ public class ManualControl {
       throw new HeaterAlreadyOnException("Light is already on, maybe you need to off?");
     }
     // TODO записать в MQTT
-    // TODO сделать запись о вкл света в бд
+    makeSendLog(true, 7, new Timestamp(System.currentTimeMillis()), 1L);
     greenhouses.getControl().setLightEnabled(true);
   }
 
   // выключить свет
+  // command 8
   @Transactional
   public void lightOff(Long greenhouseId) {
     Greenhouses greenhouses = validationService.getGreenhouseOrThrow(greenhouseId);
@@ -158,11 +178,12 @@ public class ManualControl {
       throw new HeaterAlreadyOffException("Light is already off, maybe you need to on?");
     }
     // TODO записать в MQTT
-    // TODO сделать запись о вкл света в бд
+    makeSendLog(true, 8, new Timestamp(System.currentTimeMillis()), 1L);
     greenhouses.getControl().setLightEnabled(false);
   }
 
   // включить полив на грядке
+  // command 9
   @Transactional
   public void wateringOn(Long seedBedId) {
     SeedBeds seedBed = validationService.getSeedBedOrThrow(seedBedId);
@@ -174,11 +195,12 @@ public class ManualControl {
       throw new WateringAlreadyOnException("Watering is already on, maybe you need to off?");
     }
     // TODO записать в MQTT
-    // TODO сделать запись о вкл полив в бд
+    makeSendLog(true, 9, new Timestamp(System.currentTimeMillis()), 1L);
     seedBed.setWateringEnabled(true);
   }
 
   // выключить полив на грядке
+  // command 10
   @Transactional
   public void wateringOff(Long seedBedId) {
     SeedBeds seedBed = validationService.getSeedBedOrThrow(seedBedId);
@@ -190,7 +212,7 @@ public class ManualControl {
       throw new WateringAlreadyOnException("Watering is already off, maybe you need to on?");
     }
     // TODO записать в MQTT
-    // TODO сделать запись о выкл полив в бд
+    makeSendLog(true, 10, new Timestamp(System.currentTimeMillis()), 1L);
     seedBed.setWateringEnabled(false);
   }
 }
